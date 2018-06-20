ef11f17348adfb215570a6c56404f738037391d9
Daniel Park <abideinhope@gmail.com>
2018-06-20 22:22:00 +0900

    Created Books Reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'KJV Bible'},
+    { title: 'The Two Towers'},
+    { title: 'Eloquent Ruby'}
+  ]
+}
